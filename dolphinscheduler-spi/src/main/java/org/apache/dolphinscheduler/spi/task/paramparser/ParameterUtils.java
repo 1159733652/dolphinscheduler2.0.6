8845c3926199c6711f81efe1ae4345ff9dabd0d3
zhanghongyu <zhanghongyu1@bonc.com.cn>
2023-02-17 18:05:45 +0800

    从common中改到spi下

--- dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/task/paramparser/ParameterUtils.java
+++ dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/task/paramparser/ParameterUtils.java
@@ -72,6 +72,11 @@ public class ParameterUtils {
             // replace variable ${} form,refers to the replacement of system variables and custom variables
             parameterString = PlaceholderUtils.replacePlaceholders(parameterString, parameterMap, true);
         }
+        if (compareParamWithKey(parameterString, parameterMap)) {// 走自定义变量
+            Map<String, String> newParameterMap = new HashMap<>();
+            newParameterMap.put("current_time_millis", "15641654654654165");
+            parameterString = PlaceholderUtils.replacePlaceholders(parameterString, newParameterMap, true);
+        }
         if (parameterMap != null && null != parameterMap.get(PARAMETER_DATETIME)) {
             //Get current time, schedule execute time
             String cronTimeStr = parameterMap.get(PARAMETER_DATETIME);
@@ -84,6 +89,16 @@ public class ParameterUtils {
             return dateTemplateParse(parameterString, cronTime);
         }
         return parameterString;
+    }
+
+    //参数与key值进行比对
+    private static boolean compareParamWithKey(String parameterString, Map<String, String> parameterMap) {
+        for (Map.Entry<String, String> en : parameterMap.entrySet()) {
+            if (parameterString.equals(en.getKey())) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
--- dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/utils/NewLandCustomVariableValueUtils.java
+++ dolphinscheduler-spi/src/main/java/org/apache/dolphinscheduler/spi/utils/NewLandCustomVariableValueUtils.java
@@ -1,6 +1,6 @@
-package org.apache.dolphinscheduler.common.utils;
+package org.apache.dolphinscheduler.spi.utils;
 
-import org.apache.dolphinscheduler.common.Constants;
+import org.apache.dolphinscheduler.spi.task.TaskConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
