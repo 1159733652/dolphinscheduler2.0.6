02afd5211864721ade5164bf16e5a3c7d4f934d7
zhanghongyu <zhanghongyu1@bonc.com.cn>
2023-02-17 16:48:28 +0800

    新增自定义变量测试，加自定义类

---
+++ dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/utils/NewLandCustomVariableValueUtils.java
@@ -0,0 +1,131 @@
+package org.apache.dolphinscheduler.common.utils;
+
+import org.apache.dolphinscheduler.common.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  CustomVariableValue
+ */
+public class NewLandCustomVariableValueUtils {
+    private static final Logger logger = LoggerFactory.getLogger(NewLandCustomVariableValueUtils.class);
+
+    public Map<String,String> getVariableValues(){
+        Map<String,String> resMap = new HashMap<>();
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+        resMap.put(Constants.PRE_MON_YEAR_NUMBER,"");
+
+        return resMap;
+    }
+}
